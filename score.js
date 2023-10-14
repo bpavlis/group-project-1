a365a3020f0b3436dae808e9a9d9ab2f06f7a364
Leah Tillman <leahtillman@leahs-air.lan>
2023-10-14 10:57:43 -0500

    just adding notes and ideas to comments in js files

---
+++ score.js
@@ -0,0 +1,17 @@
+/*
+-if modal for instructions on navbar, the js will need to be on all three files for consistency 
+-this is where we will need to focus on local storage:
+ a) using JSON and localStorage to successfully save input data
+ b) dynamically diplaying whatever is already stored in local while displaying new scored in real time 
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ script.js
@@ -0,0 +1,29 @@
+/* branch name js-pseudo
+notes
+-cards and styling done through html and css for the most part
+-will need to start by defining variables with query selectors 
+-will need to isolate the data points we want from the API 
+-create a function that 
+    a) randomly pulls player data from API array
+    b) displays on the page using a for loop
+    c) will need variables that specifically signify the number value at any given index
+-separate function for user choice- this will contain logic for keeping score, and if statements to make sure the correct value is being attributed during gameplay. make this an event listener function?
+-at end of game, we will need some kind of button that leads to scoreboard page? do we want the user to input their name on this window, or the score window? to discuss
+
+-if modal for instructions on navbar, the js will need to be on all three files for consistency 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
